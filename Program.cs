f4f19b947dd4dffcadb18b7f99aa9fe5c79b360e
Teah <166519031+St10440926@users.noreply.github.com>
2025-10-22 23:18:25 +0200

    commit 12: references added

--- Program.cs
+++ Program.cs
@@ -16,3 +16,45 @@ app.MapControllerRoute(
 );
 
 app.Run();
+
+/*
+------------------------------------------------------------
+REFERENCES
+------------------------------------------------------------
+
+Microsoft (2024) ASP.NET Core MVC overview. Microsoft Learn. 
+Available at: https://learn.microsoft.com/en-us/aspnet/core/mvc/overview 
+(Accessed: 22 October 2025).
+
+Microsoft (2024) File uploads in ASP.NET Core. Microsoft Learn. 
+Available at: https://learn.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads 
+(Accessed: 22 October 2025).
+
+Microsoft (2024) System.Text.Json Namespace (Serialization and Deserialization). 
+Microsoft Docs. 
+Available at: https://learn.microsoft.com/en-us/dotnet/api/system.text.json 
+(Accessed: 22 October 2025).
+
+Microsoft (2024) System.Security.Cryptography Namespace (AES Encryption). 
+Microsoft Docs. 
+Available at: https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography 
+(Accessed: 22 October 2025).
+
+Microsoft (2024) Unit testing C# code with MSTest. Microsoft Learn. 
+Available at: https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-mstest 
+(Accessed: 22 October 2025).
+
+Microsoft (2024) TempData, ViewData, and ViewBag in ASP.NET Core MVC. 
+Microsoft Learn. 
+Available at: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/app-state 
+(Accessed: 22 October 2025).
+
+TutorialsTeacher (2024) ASP.NET Core MVC – Controllers, Views and Models Overview. 
+TutorialsTeacher. 
+Available at: https://www.tutorialsteacher.com/core/aspnet-core-mvc 
+(Accessed: 22 October 2025).
+
+------------------------------------------------------------
+End of References
+------------------------------------------------------------
+*/
